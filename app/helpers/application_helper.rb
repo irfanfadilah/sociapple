99a689e752a6c429e0e6260f28f26d54452eb1df
Irfan Fadilah <irfan@41studio.com>
2015-10-30 02:32:59 +0700

    update layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def like_label(status)
     current_user.liked?(status) ? "Unlike" : "Like"
   end
+
+  def is_active_tab?(path)
+    "active" if current_page? path
+  end
 end
